557ddabb339926337c97bc6ef56e24c91df249e3
jaboulos <jab12288@gmail.com>
2021-03-14 16:19:03 -0700

    Fix caesarCipher spelling in test cases

--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -6,19 +6,19 @@ describe('caesarCipher', () => {
     expect(caesarCipher('', 1)).toEqual('');
   });
   it('returns input string if no shift value is given.', () => {
-    expect(caeserCipher('ABC DEF')).toEqual('ABC DEF');
+    expect(caesarCipher('ABC DEF')).toEqual('ABC DEF');
   });
   it('returns input string if shift value is 0.', () => {
-    expect(caeserCipher('ABC DEF', 0)).toEqual('ABC DEF');
+    expect(caesarCipher('ABC DEF', 0)).toEqual('ABC DEF');
   });
   it('shifts the string right if shift input is positive.', () => {
-    expect(caeserCipher('ABC DEF', 1)).toEqual('BCD EFG');
+    expect(caesarCipher('ABC DEF', 1)).toEqual('BCD EFG');
   });
   it('shifts the string left if shift input is negative.', () => {
-    expect(caeserCipher('ABC DEF', -1)).toEqual('ZAB CDE');
+    expect(caesarCipher('ABC DEF', -1)).toEqual('ZAB CDE');
   });
-  it('shifts the string to the right correctly if shift value is greater than 26.', () => {
-    expect(caeserCipher('ABC DEF', 27)).toEqual('BCD, EFG');
+  it('shifts the string to the right correctly if shift value is greater than 25.', () => {
+    expect(caesarCipher('ABC DEF', 27)).toEqual('BCD EFG');
   });
   it('shifts correctly with all lowercase string characters and a positive shift input.', () => {
     expect(caesarCipher('abc def', 1)).toEqual('bcd efg');
@@ -27,10 +27,12 @@ describe('caesarCipher', () => {
     expect(caesarCipher('abc def', -1)).toEqual('zab cde');
   });
   it('shifts correctly when input string has both lower and uppercase characters.', () => {
-    expect(caeserCipher('aBc DeF', 1)).toEqual('bCd EfG');
+    expect(caesarCipher('aBc DeF', 1)).toEqual('bCd EfG');
   });
-  it(
-    'returns an empty string when input string contains characters that are not letters or whitespaces.'
-  );
-  expect(caesarCipher('A&C D$F')).toEqual('');
+  it('shifts correctly with a shift value less than -25', () => {
+    expect(caesarCipher('abc def', -26)).toEqual('abc def');
+  });
+  it('returns an empty string when input string contains characters that are not letters or whitespaces.', () => {
+    expect(caesarCipher('A&C D$F')).toEqual('');
+  });
 });
